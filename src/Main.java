3edf00a2aad243ba51aa99377915ea17ba6c28b4
Ayush Kumar <ayushoppenehiemer@gmail.com>
2024-09-01 01:44:41 +0530

    added some board feature

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import tictactoe.TicTacToe;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("TIC TAC TOE Game Using CLI");
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.displayBoard();
+        ticTacToe.placeMark(0,0,'x');
+        ticTacToe.placeMark(2,1,'o');
+        ticTacToe.displayBoard();
         }
     }
